7e27979ce6321057cefc691a1283e1e77091315b
Delice Lydia <Delydia84@gmail.com>
2021-06-30 11:40:50 +0200

    Initialize redux store
    - Created initial state objects
    - Used math.random on each ID
    - Render App component wrapped in the Provider
    - Passed store down to the components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import App from './Components/App';
+import combineReducers from './Reducers/index';
 import './index.css';
-import App from './App';
+
+const state = {
+  books: [
+    {
+      id: Math.random(),
+      title: 'Americanah',
+      category: 'story',
+    },
+    {
+      id: Math.random(),
+      title: 'Eloquent-javascript',
+      category: 'lerning',
+    },
+  ],
+};
+
+const store = createStore(combineReducers, state);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
