f632dfc1b5a9d65c8a005137f8df9d9856e01d46
Delice Lydia <Delydia84@gmail.com>
2021-06-30 10:15:52 +0200

    Add book reducer

---
+++ src/Reducers/books.js
@@ -0,0 +1,14 @@
+import { CREATE_BOOK, REMOVE_BOOK } from '../Actions/ActionTypes';
+
+const bookReducer = (initialState = [], action) => {
+    switch (action.type) {
+        case CREATE_BOOK:
+            return [...initialState, action.book];
+        case REMOVE_BOOK:
+            return initialState.filter((book) => book !== action.book);
+        default:
+            return initialState;
+    }
+};
+
+export default bookReducer;
