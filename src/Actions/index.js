bba0ce132e7ec979cdb555a400e0d46be4040d06
Delice Lydia <Delydia84@gmail.com>
2021-06-30 09:57:47 +0200

    Defined CREATE_BOOK and REMOVE_BOOK actions

---
+++ src/Actions/index.js
@@ -0,0 +1,13 @@
+import { CREATE_BOOK, REMOVE_BOOK } from "./ActionTypes";
+
+const createBook = (book) => ({
+    type: CREATE_BOOK,
+    book
+});
+
+const removeBook = (book) => ({
+    type: REMOVE_BOOK,
+    book
+});
+
+export {createBook, removeBook};
