4f3442f0cb0fb9a940e427ae5d7d819e9c037c72
Delice Lydia <Delydia84@gmail.com>
2021-06-30 10:04:17 +0200

    Added Book component

---
+++ src/Components/Book.js
@@ -0,0 +1,25 @@
+import PropTypes from 'prop-types';
+
+const Book = (book) => (
+    <tr id={book.id}>
+        <td>
+        { book.id }
+        </td>
+        <td>
+        { book.title }
+        </td>
+        <td>
+        { book.category }
+        </td>
+    </tr>
+);
+
+export default Book;
+
+Book.propTypes = {
+    book: PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      category: PropTypes.string.isRequired,
+    }).isRequired,
+  };
