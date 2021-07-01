3c98d90e6fd0c8d182d7c996809196ae068b65df
Delice Lydia <Delydia84@gmail.com>
2021-07-01 11:59:15 +0200

    Made bookform component stateful
    - Store title and book category in its own state
    - Implement handleChange method

--- src/Containers/BooksForm.js
+++ src/Containers/BooksForm.js
@@ -1,7 +1,25 @@
-import React from 'react';
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import { createBook } from '../Actions/index';
 
-const BooksForm = () => {
+const BooksForm = (props) => {
   const bookCategories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-fi'];
+  const initialState = {
+    title: '',
+    category: '',
+    id: null,
+  };
+  const [book, setBook] = useState(initialState);
+
+  const handleChange = (event) => {
+    event.preventDefault();
+    setBook({
+      ...book,
+      [event.target.name]: event.target.value,
+      id: Math.floor(Math.random() * 100),
+    });
+  };
+
   return (
     <form>
       <input type="text" id="title" />
