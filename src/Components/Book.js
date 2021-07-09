24f873f46d964c6ce86331c31ca55d79bf9ae64b
Delice Lydia <Delydia84@gmail.com>
2021-07-09 11:48:59 +0200

    Add circular progress bar

--- src/Components/Book.js
+++ src/Components/Book.js
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 
 const Book = ({ book, handleRemoveBook }) => (
   <main className="book-panel flex">
